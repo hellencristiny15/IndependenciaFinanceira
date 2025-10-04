8ce91053e25a2d24f406dd0dd2a33986f3d39af2
Hellen <cristinyhellen75@gmail.com>
2025-10-03 22:10:01 -0300

    Finalizada

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+decimal gastosMensais, valorAInvestir;
+double dividendYield;
+
+Console.WriteLine("--- Vivendo de dividendos ---\n");
+Console.Write("Gastos mensis (em R$)...:");
+gastosMensais = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimento de dividendos (%)...:");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+
+valorAInvestir = gastosMensais / Convert.ToDecimal(dividendYield / 100);
+
+
+Console.WriteLine($"\nPara receber {gastosMensais:C} por mês com rendimentos de {dividendYield:N1}% você precisa ter investdo {valorAInvestir:C}.");
